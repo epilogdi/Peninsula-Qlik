f09c5c87fb0a11bcf3aeaed96aef9e5ef4957708
Danielsierra34 <danielsierra34@gmail.com>
2024-05-23 21:30:31 +0000

    se agrego el test te viewid unitario

--- cronjobs/googleAnalytics/test.php
+++ cronjobs/googleAnalytics/test.php
@@ -13,20 +13,21 @@ include "$path/vendor/autoload.php";
 include "$path/includes/mongo.php";
 include "$path/includes/googleAnalytics.php";
 
+$view_id = $_GET["viewid"];
+
 
 $database = "GoogleAnalytics";
-$controlHistorico = "Control-Historico";
-$controlDiario = "Control-Diario";
-$projects = $mongoClient->$database->$controlHistorico->find(["enabled"=>true]);  
-foreach ($projects as $project) {  
-  echo json_encode($project);
-  $response = extractAnalytics($project,'2020-01-01','yesterday');
-  $insert = getData($response,$project);
-  echo json_encode($insert);
-  if (count($insert) > 0) {
-    echo json_encode($insert);
-  }
-}
+$start = microtime(true);
+$dateStart = date('Y-m-d H:i:s');
+$collection = "Control-Diario";
+
+$project = new stdClass();
+$project->viewId = $view_id;
+
+
+$response = extractAnalytics($project,'yesterday','today');
+$insert = getData($response,$project);
+echo json_encode($insert);
 
 
 
