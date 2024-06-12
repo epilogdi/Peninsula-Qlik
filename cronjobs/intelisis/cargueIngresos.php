79740ffff97de95c03c22c2dbfae5f168085e517
danielsierra34 <danielsierra34@gmail.com>
2024-06-12 15:20:48 +0000

    se agrego el script para cargue de ingresos

---
+++ cronjobs/intelisis/cargueIngresos.php
@@ -0,0 +1,49 @@
+<?php
+
+if($_SERVER["DOCUMENT_ROOT"]){
+  $path = $_SERVER["DOCUMENT_ROOT"];
+}else{ 
+  $name = basename($_SERVER["PHP_SELF"]);
+  $path = str_replace($name, "", $_SERVER["PHP_SELF"]); 
+  $path = str_replace("/cronjobs/intelisis/", "", $path);
+}
+
+include "$path/environment.php";
+include "$path/vendor/autoload.php";
+include "$path/includes/mongo.php";
+
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
+$filename = '../../../ingresos.xlsx';
+$spreadsheet = IOFactory::load($filename);
+$worksheet = $spreadsheet->getActiveSheet();
+$rows = $worksheet->toArray();
+
+$rotulos = array();
+$objetos = array();
+foreach ($rows as $key=>$row) {
+  if($key == 0){
+    $rotulos = $row;
+  }else{
+    $obj = new stdClass();
+    $columnas = $row;
+    foreach ($columnas as $key=>$columna) {
+      if (str_contains($columna, '$')) {
+        $columna = str_replace(" ", "", $columna);
+        $columna = str_replace("$", "", $columna);
+        $columna = str_replace(",", "", $columna);
+        $columna = floatval($columna);
+      }
+      $xx=$rotulos[$key];
+      $obj->$xx=$columna;      
+    }
+    array_push($objetos, $obj);
+  }
+}
+
+$database = "Cobranzas";
+$collection = "Ingresos";
+$mongoClient->$database->$collection->insertMany($objetos);
+
+
+?>
